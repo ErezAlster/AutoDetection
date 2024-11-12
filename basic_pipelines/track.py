298e60f7682285475088f4748755bc2df96d8cad
Erez Alster <erez@alster.co.il>
2024-11-12 13:46:12 +0200

    track person

--- basic_pipelines/track.py
+++ basic_pipelines/track.py
@@ -1,6 +1,7 @@
 
 import threading
 from PCA9685 import PCA9685
+import time
 from systemd import journal
 import supervision as sv
 
@@ -14,31 +15,38 @@ pwm = PCA9685()
 pwm.setPWMFreq(50)
 
 currentMM = 5
-currentYAngleView = 110
-currentXAngleView = 70
+currentXAngleView = 53
 imageWidth = 1280
-imageHeight = 720
 currentXAngle = 90
-currentYAngle = 90
-pwm.setRotationAngle(0, currentYAngle)
+currentYAngle = 100
+pwm.setRotationAngle(0, currentXAngle)
+pwm.setRotationAngle(1, currentYAngle)
 
 
 def trackCamera(bbox):
-    global currentMM, currentXAngleView, imageWidth, imageHight, currentXAngle
+    global currentMM, currentXAngleView, imageWidth, currentXAngle,didit
     xmin, ymin, xmax, ymax = bbox[0], bbox[1], bbox[2], bbox[3]
-    bbox_x_center = xmax - xmin/2
+    bbox_x_center = (xmax + xmin)/2
     
     mXAngleChange = 90 + (currentXAngleView/2)
-    movetopixel = (imageWidth / 2) - bbox_x_center
+    movetopixel =  bbox_x_center - (imageWidth / 2)
     m = (imageWidth/2) / (mXAngleChange-90)
-
-    print(movetopixel / m)
-    currentXAngle = (movetopixel/m) + currentXAngle
-
-    log(f'{currentXAngle}')
-
-    if currentXAngle<0:
-        currentXAngle = 0
-    if currentXAngle>180:
-        currentXAngle = 180
+    diff = movetopixel/(m*15)
+    print(diff)
+    if(abs(diff) > 0.2):
+        prevXAngle = currentXAngle
+        currentXAngle = currentXAngle - diff
+        if currentXAngle<0:
+            currentXAngle = 0
+        elif currentXAngle>180:
+            currentXAngle = 180
     pwm.setRotationAngle(0, currentXAngle)
+
+
+
+def doit():
+    while True:
+        pwm.setRotationAngle(0, currentXAngle)
+
+#hread = threading.Thread(target=doit)
+#thread.start()
