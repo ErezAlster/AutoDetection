8e63af84fba531a902dbdb3aa7263e1a993dc3c7
Erez Alster <erez@alster.co.il>
2024-11-20 08:39:35 +0000

    keep ball in the center of the image

--- basic_pipelines/track.py
+++ basic_pipelines/track.py
@@ -1,9 +1,15 @@
 
+from multiprocessing import Process
+import multiprocessing as mp 
 import threading
-from PCA9685 import PCA9685
 import time
+from PCA9685 import PCA9685
 from systemd import journal
 import supervision as sv
+import pyjoystick
+from pyjoystick.sdl2 import Key, Joystick, run_event_loop
+import signal
+from multiprocessing.connection import Listener
 
 def log(message):
     journal.write(message)
@@ -16,37 +22,104 @@ pwm.setPWMFreq(50)
 
 currentMM = 5
 currentXAngleView = 53
+currentYAngleView = 70
 imageWidth = 1920
-currentXAngle = 90
-currentYAngle = 100
+imageHeight = 1080
+currentXAngle = 91
+panFactor = 0
+manualX = False
+manualY = False
+tiltFactor = 0
+currentYAngle = 90
 pwm.setRotationAngle(0, currentXAngle)
 pwm.setRotationAngle(1, currentYAngle)
 
 
-def trackCamera(bbox):
-    global currentMM, currentXAngleView, imageWidth, currentXAngle,didit
-    xmin, ymin, xmax, ymax = bbox[0], bbox[1], bbox[2], bbox[3]
-    bbox_x_center = (xmax + xmin)/2
-    
-    mXAngleChange = 90 + (currentXAngleView/2)
-    movetopixel =  bbox_x_center - (imageWidth / 2)
+def handleServoMovment(centerPixel, axisPixels, angleView):
+    mXAngleChange = 90 + (angleView/2)
+    movetopixel =  centerPixel - (axisPixels / 2)
     m = (imageWidth/2) / (mXAngleChange-90)
-    diff = movetopixel/(m*15)
-    print(diff)
-    if(abs(diff) > 0.2):
-        prevXAngle = currentXAngle
-        currentXAngle = currentXAngle - diff
-        if currentXAngle<0:
-            currentXAngle = 0
-        elif currentXAngle>180:
-            currentXAngle = 180
-    pwm.setRotationAngle(0, currentXAngle)
+    return movetopixel/(m*15)
+
+def setXaxis(angle):
+
+    if(angle<0):
+        angle = 0
+    elif (angle>180):
+        angle = 180
+
+    pwm.setRotationAngle(0, angle)
+
+def setYaxis(angle):
+    if(angle<0):
+        angle = 0
+    elif (angle>180):
+        angle = 180
+    
+    pwm.setRotationAngle(1, angle)
 
 
+def trackCamera(bbox):
+    global currentMM, currentXAngleView, imageWidth, imageHeight, currentXAngle,currentYAngle
+    xmin, ymin, xmax, ymax = bbox[0], bbox[1], bbox[2], bbox[3]
+    if(not manualX):
+        #handle x axix, make the ball in the center of the frame
+        bbox_x_center = (xmax + xmin)/2
+        currentXAngle -= handleServoMovment(centerPixel=bbox_x_center, axisPixels=imageWidth, angleView=currentXAngleView)
+        setXaxis(currentXAngle)
 
-def doit():
-    while True:
-        pwm.setRotationAngle(0, currentXAngle)
+    if(not manualY):
+        bbox_y_center = (ymax + ymin)/2
+        currentYAngle += handleServoMovment(centerPixel=bbox_y_center, axisPixels=imageHeight, angleView=currentYAngleView)
+        setYaxis(currentYAngle)
 
-#hread = threading.Thread(target=doit)
-#thread.start()
+def key_received(key):
+    global panFactor, tiltFactor, manualX, manualY
+    match key:
+        case "Axis 0":
+            panFactor = key.value
+        case "-Axis 0":
+            panFactor = key.value
+        case "-Axis 1":
+            tiltFactor = key.value
+        case "Axis 1":
+            tiltFactor = key.value
+        case "Button 12":
+            if(key.value == 1):
+                manualX = not manualX
+                manualY = not manualY
+        case "Button 5":
+            if(key.value == 1):
+                manualX = not manualX
+                manualY = not manualY
+
+def signal_term_handler(sigNum, frame):
+    exit(0)
+signal.signal(signal.SIGTERM, signal_term_handler)
+
+
+mngr = pyjoystick.ThreadEventManager(event_loop=run_event_loop, handle_key_event=key_received)
+mngr.start()
+
+def joystick_worker():
+    global currentXAngle,currentYAngle, panFactor, tiltFactor
+    while (True):
+        if(manualX):
+            factor = (pow(abs(panFactor), 2)) / 7
+            if(panFactor < 0):
+                factor = factor * -1
+            currentXAngle = currentXAngle - factor
+            setXaxis(currentXAngle)
+
+        if(manualX):
+            factor = (pow(abs(tiltFactor), 2)) / 20
+            if(tiltFactor < 0):
+                factor = factor * -1
+            currentYAngle = currentYAngle + factor
+            setYaxis(currentYAngle)
+
+
+tolat = mp.Queue() 
+
+#p = Process(target=joystick_worker)
+#p.start()
--- basic_pipelines/track_ball.py
+++ basic_pipelines/track_ball.py
@@ -7,6 +7,9 @@ from track import trackCamera
 tracker = sv.ByteTrack()
 last_track_id = None
 
+
+CLASS_ID_TO_TRACK = 1
+
 def extract_detections(hailo_detections, w: int, h: int) -> Dict[str, np.ndarray]:
     xyxy: List[np.ndarray] = []
     confidence: List[float] = []
@@ -16,7 +19,7 @@ def extract_detections(hailo_detections, w: int, h: int) -> Dict[str, np.ndarray
     for detection in hailo_detections:
         class_id_num = detection.get_class_id()
         #take only person
-        if class_id_num==1:
+        if class_id_num>-1:
             bbox = detection.get_bbox()
             score = detection.get_confidence()
             xyxy.append([bbox.xmin()* w, bbox.ymin() * h, bbox.xmax() * w, bbox.ymax() * h])
@@ -31,16 +34,41 @@ def extract_detections(hailo_detections, w: int, h: int) -> Dict[str, np.ndarray
         "num_detections": num_detections,
     })
 
+def sortByDistanceFromXCenter(bbox):
+    xmin, xmax  = bbox[0], bbox[2]
 
-def postprocess_detections(detections: Dict[str, np.ndarray],) -> np.ndarray:
+    #get the pixel distance from the center of the image to the object
+    #tracked objects should be closer to the distance as much as we can
+    return abs((1980 / 2) - ((xmax + xmin)/2))
+
+def postprocess_detections(detections: Dict[str, np.ndarray],):
+    global last_track_id
     if detections["num_detections"]>0:
         sv_detections = sv.Detections(
             xyxy=detections["xyxy"],
             confidence=detections["confidence"],
             class_id=detections["class_id"],
         )
-
-
+        
+        focus_bbox = None
         sv_detections = tracker.update_with_detections(sv_detections)
 
-        trackCamera(detections["xyxy"][0])
+        #Previous 
+        if(last_track_id is not None):
+            map_result = {item.external_track_id: item for item in tracker.tracked_tracks}
+            tracked_bbox = map_result.get(last_track_id)
+            if(tracked_bbox is not None):
+                focus_bbox =  map_result.get(last_track_id).tlbr
+            else:
+                last_track_id = None #No object was found for last tracking id
+
+        #Find best match object to center at incase not object were found before
+        if(last_track_id is None):
+            potentials_detections = [detection for detection in sv_detections if detection[3] == CLASS_ID_TO_TRACK]
+            potentials_detections.sort(key=sortByDistanceFromXCenter)
+            if(len(potentials_detections)>0):
+                focus_bbox = potentials_detections[0][0]
+                last_track_id = potentials_detections[0][4] #Get track id
+
+        if(focus_bbox is not None):
+            trackCamera(focus_bbox)
